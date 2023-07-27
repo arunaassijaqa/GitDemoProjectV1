1c91d954670f59dfca9d811558affbfc5b8ca8d7
aruna <aruna.assijaqa@gmail.com>
2023-07-26 20:33:22 -0400

    Commiting Test2 to Branch2

---
+++ src/Test2.java
@@ -0,0 +1,16 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry ="yes";
+		if(hungry=="yes")
+			System.out.println(" Updated Eat Somthing");
+		else {
+			System.out.println(" Updated Do Work ...");
+		}
+		
+
+	}
+
+}
